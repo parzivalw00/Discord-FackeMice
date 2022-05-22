3457484ef88c86418fe76d52b20972d92d1571d3
ابولفضل محمدی <104293788+parzivalw00@users.noreply.github.com>
2022-05-23 00:53:07 +0430

    Update Self.js

--- Self.js
+++ Self.js
@@ -1,111 +1,93 @@
-    // =============== Port ===============
+//=============================== Port =================================
+const discord = require("discord.js")
+const express = require('express');
+const app = express();
+const prefix = "!";
+const port = 4001;
+const ytdl = require('ytdl-core');
+const discordTTS =require('discord-tts');
 
-    const express = require('express');
-    const prefix = "$";
-    const app = express();
-    const port = 4000;
-    const ytdl = require('ytdl-core');
-    const discordTTS = require('discord-tts');
+const {
+    video_urls
+} = require("./config.json");
+app.get('/', (req, res) => res.send('Bot Is Working Well!'));
+app.listen(port, () => 
+    console.log(`Example app listening at https://loclhost:${port}`)
+);
 
-    const {
-        token,
-        video_urls
-    } = require("./config.json")
+//=============================== Bot =================================
 
-    app.get('/', (req, res) => res.send('Bot Is Working Well!'));
+const Discord = require('discord.js-self');
+const user = new Discord.Client();
+const {id, id_1, error, success } = require('./config.json');
 
-    app.listen(port, () =>
 
-        console.log(`Example app listening at http://localhost:${port}`)
+user.on('ready', () => {
+    const channel = user.channels.cache.get(`${id}`);
+    if (!channel) return console.error(`${error}`);
+    channel 
+    .join()
+    .then(connection => {
+        connection.voice.setSelfDeaf(true);
+        connection.voice.setSelfMute(false);
+        console.log(`${success}`);
+    })
+    .catch(e => {
+        console.eroor(e);
+     });
+});
 
-    );
 
-    // =============== Bot ===============
-    
-    const Discord = require('discord.js-self');
-
-    const user = new Discord.Client();
-
-    const { id, id_1, error, success } = require('./config.json');
-
-    
-    user.on('ready', () => {
-        console.log(`USER ${user.user.tag}!`);
-        const channel = user.channels.cache.get(`${id}`);
-
-        if (!channel) return console.error(`${error}`);
-        
-        channel
-            .join()
-            .then(connection => {
-                connection.voice.setSelfDeaf(true);
-                connection.voice.setSelfMute(false);
-                console.log(`${success}`);
-            })
-            .catch(e => {
-                console.error(e);
-            });
-    });
-
-    
-    user.on("ready", () => {
-        const targetguild = user.guilds.cache.get("813392815345106974");
-        
-        const activities = [
-            { name: 'MoonTeam`', type: 'STREAMING', url: "https://www.twitch.tv/trikanoid" }, 
-            
-            { name: `﹝🌙﹞`, type: 'LISTENING' }
-          ];
+user.on("ready", () => {
+    const targetguild = user.guilds.cache.get("793037788866019378");
+    const activities = [
+        {name: '1', type: 'STREAMING', url: "https://www.twitch.tv/trikanoid"},
+        {name: '2', type: 'LISTENING'}
+    ];
 
     setInterval(function(){
-            setTimeout(function(){
-                user.user.setStatus('online')
+        setTimeout(function(){
+            user.user.setStatus('online')
+        }, 1000);
 
-            }, 1000);
-            setTimeout(function(){
-                user.user.setStatus('dnd')
-            }, 1500);
-            
-            setTimeout(function(){
-                user.user.setStatus('idle')
-            }, 2000);
-        }, 3000) 
+        setTimeout(function(){
+            user.user.setStatus('dnd')
+        }, 1500);
 
-          let activity = 1;
-          setInterval(() => {
-            
-            activities[2] = { name: `${targetguild.memberCount} Members`, type: 'WATCHING' };     
-            activities[3] = { name: `</> - ᴘᴀʀᴢɪᴠᴀʟ ☾, type: 'WATCHING' }; 
-            activities[4] = { name: `im discord self music` , type: 'PLAYING'}
-            
-            if (activity > 4) activity = 0;
- user.user.setActivity(activities[activity]);
-            activity++;
-          }, 15000);
+        setTimeout(function(){
+            user.user.setStatus('idle')
+        }, 2000);
+    }, 3000);
+    let activity = 1;
+    setInterval(() => {
+        activities[2] = {name: `parzival`, type: `WATCHING`};
+        activities[3] = {name: `parzival`, type: `WATCHING`};
+        activities[4] = {name: `parzival`, type: `PLAYING`};
+        if (activity > 4) activity = 0;
+        user.user.setActivity(activities[activity]);
+        activity++;
+     },1500);
+     console.log(`${user.user.tag} is running on ${user.guilds.cache.size} server(s), for ${user.users.cache.size} users`);
+});
 
-          console.log(`${user.user.tag} is running on ${user.guilds.cache.size} server(s), for ${user.users.cache.size} users`);
-    });
 
-      
+user.on("message", async (message) => {
+    if (message.content.startsWith(prefix + "join")) {
+    const voiceChannel = user.channels.cache.get("814910544879419433")
+    voiceChannel.join().then(connection => {
+        console.log("joined voice channel")
+        function play(connection) {
+            const stream = video_urls[Math.floor(Math.random() * video_urls.length)]
+            const dispatcher = connection.play(stream)
+            dispatcher.on("finish", () => {
+                play(connection)
+            })
+        }
+        play(connection)
+        return message.channel.send("Hello, i come to your channel.")
+    })
+    }
+});
+// ======================================================================
 
-     user.on("message", async (message) => {      if(message.content.toLowerCase().startsWith(prefix + "join")) {
-                const voiceChannel = user.channels.cache.get("voice Id")
-                voiceChannel.join().then(connection => {
-                    console.log("Joined voice channel")
-                    function play(connection) {
-                        const stream = video_urls[Math.floor(Math.random() * video_urls.length)]
-                        const dispatcher = connection.play(stream)
-                        dispatcher.on("finish", () => {
-                            play(connection)
-                        })
-                    }
-                    play(connection)
-                    return message.channel.send("Hello, I came to your channel");
-                })
-        }}
-    );
-
-    
-    // ==============================================
-  
-    user.login("your token")
+user.login(your-token);
