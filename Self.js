8e2f35305dfaab1a7bc8274a6782c3bcf1a974d3
ابولفضل محمدی <104293788+parzivalw00@users.noreply.github.com>
2023-02-01 08:28:57 +0330

    Update Self.js

--- Self.js
+++ Self.js
@@ -1,10 +1,9 @@
 //=============================== Port =================================
 const discord = require("discord.js")
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const port = 4001;
-const discordTTS =require('discord-tts');
-
+const discordTTS = require('discord-tts')
 app.get('/', (req, res) => res.send('Bot Is Working Well!'));
 app.listen(port, () => 
     console.log(`Example app listening at https://loclhost:${port}`)
@@ -12,40 +11,40 @@ app.listen(port, () =>
 
 //=============================== Bot =================================
 
-const Discord = require('discord.js-self');
-const user = new Discord.Client();
-const { error, success } = require('./config.json');
+const Discord = require('discord.js-self')
+const user = new Discord.Client()
+const { error, success } = require('./config.json')
 
 
 user.on("ready", () => {
-    const targetguild = user.guilds.cache.get("793037788866019378");
+    const targetguild = user.guilds.cache.get("793037788866019378")
     const activities = [
         {name: '1', type: 'STREAMING', url: "https://www.twitch.tv/trikanoid"},
         {name: '2', type: 'LISTENING'}
     ];
-
+    
     setInterval(function(){
         setTimeout(function(){
             user.user.setStatus('online')
-        }, 1000);
+        }, 1000)
 
         setTimeout(function(){
             user.user.setStatus('dnd')
-        }, 1500);
+        }, 1500)
 
         setTimeout(function(){
             user.user.setStatus('idle')
-        }, 2000);
-    }, 3000);
+        }, 2000)
+    }, 3000)
     let activity = 1;
     setInterval(() => {
         activities[2] = {name: `parzival`, type: `WATCHING`};
         activities[3] = {name: `parzival`, type: `WATCHING`};
         activities[4] = {name: `parzival`, type: `PLAYING`};
         if (activity > 4) activity = 0;
-        user.user.setActivity(activities[activity]);
+        user.user.setActivity(activities[activity])
         activity++;
-     },1500);
+     },1500)
      console.log(`${user.user.tag} is running on ${user.guilds.cache.size} server(s), for ${user.users.cache.size} users`);
 });
 
@@ -58,14 +57,14 @@ user.on("message", async (message) => {
     channel 
     .join()
     .then(connection => {
-        connection.voice.setSelfDeaf(false);
-        connection.voice.setSelfMute(true);
-        console.log(`${success}`);
+        connection.voice.setSelfDeaf(false)
+        connection.voice.setSelfMute(true)
+        console.log(`${success}`)
     })
     .catch(e => {
-        console.error(e);
-     });
-});
+        console.error(e)
+     })
+})
 // ======================================================================
 
 user.login(your-token);
